6742f66dbc74bfb8e3e7d02f5015cc7a9e33b952
darklordhrp <125478858+darklordhrp@users.noreply.github.com>
2023-05-09 13:25:58 +0545

    Add files via upload

---
+++ Week11/script.js
@@ -0,0 +1,22 @@
+document.getElementsByClassName("exercise")[0].innerHTML=("tyrion lanister")
+document.getElementsByClassName("exercise")[0].style.cssText=("background-color:red; color:black; text-align:center")
+
+document.getElementById("parent1").style.cssText=("background-color:aqua; color:brown")
+document.getElementsByClassName("exercise")[2].innerHTML=("i a ok");
+document.getElementsByTagName("h1")[2].style.cssText=
+("background-color:blue; color:black");
+
+document.querySelector("h2").style.cssText=
+("text-align:center; text-transform:uppercase")
+document.querySelector("#hi").style.cssText=
+("text-align:center; text-transform:uppercase")
+
+const chane =document.querySelectorAll(h3);
+for(i=0;i<=ChannelMergerNode.length;i++){
+    chane[i].innerHTML=("nbchjb jna am");
+}
+function myFunction(){
+    document.open();
+    document.write("<h1>Hello World</h1>");
+    document.close();
+}
---
+++ Week11/script1.js
@@ -0,0 +1,20 @@
+function myFunction(){
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+    const button=document.querySelector("button");
+    if(element.classList.contains("dark-mode")){
+        button.textContent="Light-mode";
+
+
+    }else{
+        button.textContent="Dark-Mode";
+    }
+}
+
+const textchange=document.querySelector("#change");
+const values=["movies watcher","gamer","programmer"];
+let counter=0;
+setInterval(()=>{
+    textchange.textContent = values[counter];
+    counter = (counter+1)%values.length;
+},3000);
